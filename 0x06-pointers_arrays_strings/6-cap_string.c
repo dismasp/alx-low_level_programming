84f8dd5151415c59674a207506d892321129ceab
Dismas Albert <dismasp@gmail.com>
2023-03-01 08:40:16 -0800

    String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
 *@str: string to capitalize
 *Return:returns the capitalized string
 */
-char *cap_string(char *str)
+/*char *cap_string(char *str)
 {
 int index = 0;
 
@@ -17,4 +17,23 @@ if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' ||
 str[index] -= 32;
 }
 return (str);
+}
+*/
+char* cap_string(char* str) {
+int len = strlen(str);
+int i;
+// Capitalize the first letter of the string
+str[0] = toupper(str[0]);
+
+// Loop through the string, looking for separators
+for (i = 1; i < len; i++) 
+{
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}') 
+{
+// Capitalize the next character after the separator
+str[i+1] = toupper(str[i+1]);
+}
+}
+
+return str;
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <ctype.h>
 /**
  *void prototypes()
  *int prototypes()
