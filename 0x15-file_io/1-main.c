5854fe3a1a12e5345d8e25993df8e34b498b9c94
Dismas <dismasp@gmail.com>
2023-04-08 01:36:07 -0700

    Push

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -4,7 +4,8 @@
 
 /**
  * main - check the code
- *
+ * @ac: The number of arguments passed to the program
+ * @av: An array of strings containing the arguments
  * Return: Always 0.
  */
 int main(int ac, char **av)
